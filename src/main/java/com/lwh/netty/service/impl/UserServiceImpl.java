ec2f5de39bf649b7c11820c2fe03e2ee3b01632f
lwh <2391519868@qq.com>
2018-10-14 21:50:09 +0800

    SpringBoot/Netty仿微信聊天

--- src/main/java/com/lwh/netty/service/impl/UserServiceImpl.java
+++ src/main/java/com/lwh/netty/service/impl/UserServiceImpl.java
@@ -3,13 +3,19 @@ package com.lwh.netty.service.impl;
 import com.lwh.netty.mapper.UsersMapper;
 import com.lwh.netty.pojo.Users;
 import com.lwh.netty.service.UserService;
+import com.lwh.netty.utils.FastDFSClient;
+import com.lwh.netty.utils.FileUtils;
+import com.lwh.netty.utils.QRCodeUtils;
 import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import tk.mybatis.mapper.entity.Example;
 import tk.mybatis.mapper.entity.Example.Criteria;
+
+import java.io.IOException;
 
 /**
  * @author lwh
@@ -25,6 +31,12 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private Sid sid;
+
+    @Autowired
+    private QRCodeUtils qrCodeUtils;
+
+    @Autowired
+    private FastDFSClient fastDFSClient;
 
     @Transactional(propagation = Propagation.SUPPORTS, rollbackFor = Exception.class)
     @Override
@@ -58,11 +70,22 @@ public class UserServiceImpl implements UserService {
         //生成唯一ID
         String userId = sid.nextShort();
 
-        //TODO, 为每一个用户生成二维码
-        user.setQrcode("");
+        //为每一个用户生成二维码
+        String qrCodePath = "D://user" + userId + "qrcode.png";
+        //wechat_qrcode:[username]
+        qrCodeUtils.createQRCode(qrCodePath, "wechat_qrcode:" + user.getUsername());
+        MultipartFile qrCodeFile = FileUtils.fileToMultipart(qrCodePath);
 
+        String qrCodeUrl = "";
+
+        try {
+            qrCodeUrl = fastDFSClient.uploadQRCode(qrCodeFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        user.setQrcode(qrCodeUrl);
         user.setId(userId);
-
         usersMapper.insert(user);
         return user;
     }
