4b0b765f13bad36d4477d568ddf5299c94807f1d
lwh <2391519868@qq.com>
2018-10-16 21:25:31 +0800

    SpringBoot/Netty仿微信聊天

---
+++ src/main/java/com/lwh/netty/mapper/UsersMapperCustom.java
@@ -0,0 +1,11 @@
+package com.lwh.netty.mapper;
+
+
+import com.lwh.netty.pojo.Users;
+import com.lwh.netty.utils.MyMapper;
+
+/**
+ * 一些关联查询的SQL
+ */
+public interface UsersMapperCustom extends MyMapper<Users> {
+}
