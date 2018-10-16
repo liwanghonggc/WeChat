cf0fc6afa949fb0858fef943cf4f15b2278ad333
lwh <2391519868@qq.com>
2018-10-16 11:27:47 +0800

    SpringBoot/Netty仿微信聊天

--- src/test/java/com/lwh/netty/NettyApplicationTests.java
+++ src/test/java/com/lwh/netty/NettyApplicationTests.java
@@ -1,7 +1,10 @@
 package com.lwh.netty;
 
+import com.lwh.netty.pojo.Users;
+import com.lwh.netty.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class NettyApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
     @Test
     public void contextLoads() {
+        Users user = userService.queryUserInfoByUsername("xcj");
+        System.out.println(user.getUsername());
     }
 
 }
