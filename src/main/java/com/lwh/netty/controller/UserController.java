9b2ccb471369b7df8858dc1f549ababf444b93ee
lwh <2391519868@qq.com>
2018-10-18 21:05:50 +0800

    SpringBoot/Netty仿微信聊天

--- src/main/java/com/lwh/netty/controller/UserController.java
+++ src/main/java/com/lwh/netty/controller/UserController.java
@@ -244,7 +244,8 @@ public class UserController {
             userService.passFriendRequest(sendUserId, acceptUserId);
         }
 
-        return WeChatResult.ok();
+        List<MyFriendsVO> myFriendsVOList = userService.queryMyFriends(acceptUserId);
+        return WeChatResult.ok(myFriendsVOList);
     }
 
     /**
