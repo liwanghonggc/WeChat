d21f52a3df6a39fdb2f369d374d1cde10a7d0afd
lwh <2391519868@qq.com>
2018-10-19 09:10:47 +0800

    SpringBoot/Netty仿微信聊天

--- src/main/java/com/lwh/netty/websocket/ChatHandler.java
+++ src/main/java/com/lwh/netty/websocket/ChatHandler.java
@@ -20,19 +20,18 @@ public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame>
     /**
      * 用于记录和管理所有的客户端channel
      */
-    private static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    private static ChannelGroup users = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
-        //获取客户端传过来的消息
+        //1.获取客户端传过来的消息
         String content = msg.text();
 
-        System.out.println("接收到的数据: " + content);
-
-        for(Channel channel : clients){
-            channel.writeAndFlush(
-                    new TextWebSocketFrame("[服务器在: ]" + LocalDateTime.now() + " 接收到消息, 消息为: " + content));
-        }
+        //2.判断消息的类型,根据不同的类型处理不同的业务
+        //2.1 当WebSocket第一次open时,初始化channel时,把用户的channel和UserId关联起来
+        //2.2 聊天类型的消息,把聊天记录保存到数据库,同时标记消息的签收状态(未签收)
+        //2.3 签收消息类型,针对具体的消息进行签收,修改数据库中对应的消息的签收状态(已签收)
+        //2.4 心跳类型的消息
     }
 
     /**
@@ -44,15 +43,27 @@ public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame>
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
         Channel channel = ctx.channel();
-        clients.add(channel);
+        users.add(channel);
     }
 
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
         //当触发handlerRemoved,ChannelGroup会自动移除客户端的channel
-        //clients.remove(ctx.channel());
+        //这里我们手动remove掉
+        users.remove(ctx.channel());
+    }
 
-        System.out.println("客户端断开,channel对应的长id为: " + ctx.channel().id().asLongText());
-        System.out.println("客户端断开,channel对应的短id为: " + ctx.channel().id().asShortText());
+    /**
+     * 出现异常
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        //发生异常之后关闭channel,随后从ChannelGroup中移除
+        ctx.channel().close();
+        users.remove(ctx.channel());
     }
 }
---
+++ src/main/java/com/lwh/netty/websocket/pojo/DataContent.java
@@ -0,0 +1,16 @@
+package com.lwh.netty.websocket.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @author lwh
+ * @date 2018-10-19
+ * @desp
+ */
+public class DataContent implements Serializable {
+
+    /**
+     * Alt insert 快捷键生成序列化ID
+     */
+    private static final long serialVersionUID = -3696588780469986703L;
+}
